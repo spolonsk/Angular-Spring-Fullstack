f15d09d8db0641aebf247a2a45fccd28ce2af228
Seb <sebbpm@gmail.com>
2023-01-03 23:20:09 +0100

    backend

---
+++ backend/src/main/java/com/spolonsk/daily/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.spolonsk.daily.repository;
+
+import com.spolonsk.daily.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
